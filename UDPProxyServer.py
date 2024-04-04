7239a5792302b4663538d5852d0c177dcc987d11
Nuggitt <Phulip.nielsen@gmail.com>
2024-04-04 12:14:16 +0200

    works

--- UDPProxyServer.py
+++ UDPProxyServer.py
@@ -1,15 +1,16 @@
 from socket import *
+import requests
 
 serverPort = 10100
 serverSocket = socket(AF_INET, SOCK_DGRAM)
-
 serverAddress = ('', serverPort)
+
+apiAddress = "http://localhost:5193/api/SpeedMeasurements"
+headersArray = { "Content-Type": "application/json" }
 
 serverSocket.bind(serverAddress)
 print("The server is ready")
 while True:
     message, clientAddress = serverSocket.recvfrom(2048)
     print("Received message:" + message.decode())
-    modifiedMessage = message.decode().upper()
-    serverSocket.sendto(modifiedMessage.encode(), clientAddress)
-    print("Sent message:" + modifiedMessage)
+    requests.post(apiAddress, data=message, headers=headersArray)
